0c9e2b79bdc0b6c0135493afc2e4816a4709c1c3
308 <308@academicos.cesde.edu.co>
2022-05-05 06:52:53 -0500

    Se crea cambio 3 en rama1

--- clases/Persona.py
+++ clases/Persona.py
@@ -2,6 +2,7 @@ class Persona:
     def __init__(self):
         self.nombre =None
         self.edad = None
+        self.telefono = None
         
     def saludar():
         print('Hola soy daniel')    
