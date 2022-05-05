736a49eeb32b028fab26ecda7ac0de9e4b6935cd
jhonatan.ossa <jhonatan.ossa@ceiba.com.co>
2022-05-05 07:03:49 -0500

    Cambio 3 desde rama1 Jhonatan.Ossa

--- classes/Persona.py
+++ classes/Persona.py
@@ -2,6 +2,7 @@ class Persona:
     def __init__(self): #Constructor
         self.nombre =None
         self.edad =None
+        self.telefono =None
 
     def saludar(self):
         print("Hola, soy Jhonatan.Ossa")
